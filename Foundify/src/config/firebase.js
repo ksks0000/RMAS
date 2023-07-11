ebf0a9fab7282d86fec2ce19f16d155cae67cedf
Ksenija Seizovic <ksenija.seizovic@elfak.rs>
2023-07-11 03:43:21 +0200

    setting up with firebase

---
+++ Foundify/src/config/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBwLZYPlkSGZCADo379ABdca5BziBHOX8Y",
+    authDomain: "fuondify.firebaseapp.com",
+    projectId: "fuondify",
+    storageBucket: "fuondify.appspot.com",
+    messagingSenderId: "372221904436",
+    appId: "1:372221904436:web:155346b8254917040a6c61"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
