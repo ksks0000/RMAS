b954516143d18ee0593e6431ac4e91f913b4cfce
Ksenija Seizovic <ksenija.seizovic@elfak.rs>
2023-08-07 04:16:39 +0200

    enabled map view and user location

--- Foundify/src/screens/MapScreen.js
+++ Foundify/src/screens/MapScreen.js
@@ -1,12 +1,55 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import MapView from "react-native-maps"
+import * as Location from "expo-location"
 
 export default function MapScreen() {
+    const [currentLocation, setCurrentLocation] = useState({
+        latitude: 43.318887,
+        longitude: 21.895935
+    });
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                console.log('Permission to access location was denied');
+                return;
+            }
+            let location = await Location.getCurrentPositionAsync({});
+            setCurrentLocation({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude
+            });
+        })();
+    }, []);
+
     return (
-        <View>
-            <Text>MapScreen</Text>
+        <View style={styles.mapContainer}>
+            <MapView
+                style={styles.map}
+                initialRegion={{
+                    latitude: 43.318887,
+                    longitude: 21.895935,
+                    latitudeDelta: 0.05,
+                    longitudeDelta: 0.05,
+                }}
+                showsUserLocation={true}
+                userLocationUpdateInterval={5000}
+                followsUserLocation={true} // samo za apple?
+            />
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    mapContainer: {
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    map: {
+        width: "100%",
+        height: "100%"
+    }
+})
